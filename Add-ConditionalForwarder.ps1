2e259aed22c1c95d78837fc1945598fa54cc29cb
Gregory Bryant <89801409+GregoryWBryant@users.noreply.github.com>
2024-06-28 10:48:25 -0400

    Add files via upload

---
+++ Add-ConditionalForwarder.ps1
@@ -0,0 +1,17 @@
+﻿$MasterServers = @("127.0.0.1","127.0.0.2")
+$Domain = "contoso.com"
+
+
+$DNSServers = Get-DnsServerResourceRecord -ZoneName $Domain -RRType "NS" -Node
+
+
+foreach($DNSServer in $DNSServers) {
+    
+    $DNSServer.RecordData.NameServer
+    if($DNSServer.RecordData.NameServer -like "*don't modify*" -or $DNSServer.RecordData.NameServer -like "*don't modify*") {
+        Write-Host ("Not adding Forwadred to Azure server: " + $DNSServer.RecordData.NameServer)
+    } else { 
+        Add-DnsServerConditionalForwarderZone -ComputerName $DNSServer.RecordData.NameServer -Name "file.core.winodws.net" -MasterServers $MasterServers -PassThru
+        }
+
+}
