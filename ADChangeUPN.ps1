9321fee7a304ebb5619c228ad083fa5353992c2a
Gregory Bryant <89801409+GregoryWBryant@users.noreply.github.com>
2024-06-28 10:44:45 -0400

    Add files via upload

---
+++ ADChangeUPN.ps1
@@ -0,0 +1,9 @@
+﻿Import-Module ActiveDirectory
+$Users = Get-ADUser -Filter {UserPrincipalName -like '*contoso.local'} -Properties userPrincipalName
+
+foreach ($User in $Users) {
+    if ($User.UserPrincipalName -notlike "*Mailbox*" -and $User.UserPrincipalName -notlike "*FederatedEmail*") {
+            $NewUPN = $User.UserPrincipalName.Replace("contoso.local","contoso.com")
+            Set-ADUser -Identity $User -UserPrincipalName $NewUPN
+        }
+}
