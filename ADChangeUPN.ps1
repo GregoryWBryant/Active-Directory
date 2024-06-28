6031e8313e192220f8495a3bc9701fa17a6bd5db
Gregory Bryant <89801409+GregoryWBryant@users.noreply.github.com>
2024-06-28 10:46:10 -0400

    Delete ADChangeUPN.ps1

--- ADChangeUPN.ps1
+++
@@ -1,9 +0,0 @@
-﻿Import-Module ActiveDirectory
-$Users = Get-ADUser -Filter {UserPrincipalName -like '*contoso.local'} -Properties userPrincipalName
-
-foreach ($User in $Users) {
-    if ($User.UserPrincipalName -notlike "*Mailbox*" -and $User.UserPrincipalName -notlike "*FederatedEmail*") {
-            $NewUPN = $User.UserPrincipalName.Replace("contoso.local","contoso.com")
-            Set-ADUser -Identity $User -UserPrincipalName $NewUPN
-        }
-}
